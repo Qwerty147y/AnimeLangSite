a5aec23c3ca41f3b54e2ba5dd5c54e82e5075e62
Qwerty147y <hyacinthusww@gmail.com>
2025-08-03 01:31:00 +0600

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,60 @@
+// Данные аниме для сайта
+const animeData = [
+    {
+        id: 'demon-slayer',
+        title: 'Клинок, рассекающий демонов',
+        bannerUrl: '',
+        episodes: [
+            {
+                number: 1,
+                videoUrl: 'https://iframe.mediadelivery.net/play/474667/e89f358a-2dba-477b-8594-2c3f7cbaafae'
+            },
+            {
+                number: 2,
+                videoUrl: 'https://iframe.mediadelivery.net/play/474667/example-video-2'
+            },
+            {
+                number: 3,
+                videoUrl: 'https://iframe.mediadelivery.net/play/474667/example-video-3'
+            }
+        ]
+    },
+    {
+        id: 'attack-on-titan',
+        title: 'Атака титанов',
+        bannerUrl: 'https://images.pexels.com/photos/1666065/pexels-photo-1666065.jpeg?auto=compress&cs=tinysrgb&w=1920&h=1080&fit=crop',
+        episodes: [
+            {
+                number: 1,
+                videoUrl: 'https://iframe.mediadelivery.net/play/474667/example-video-4'
+            },
+            {
+                number: 2,
+                videoUrl: 'https://iframe.mediadelivery.net/play/474667/example-video-5'
+            }
+        ]
+    },
+    {
+        id: 'naruto',
+        title: 'Наруто',
+        bannerUrl: 'https://images.pexels.com/photos/1666065/pexels-photo-1666065.jpeg?auto=compress&cs=tinysrgb&w=1920&h=1080&fit=crop',
+        episodes: [
+            {
+                number: 1,
+                videoUrl: 'https://iframe.mediadelivery.net/play/474667/example-video-6'
+            },
+            {
+                number: 2,
+                videoUrl: 'https://iframe.mediadelivery.net/play/474667/example-video-7'
+            },
+            {
+                number: 3,
+                videoUrl: 'https://iframe.mediadelivery.net/play/474667/example-video-8'
+            },
+            {
+                number: 4,
+                videoUrl: 'https://iframe.mediadelivery.net/play/474667/example-video-9'
+            }
+        ]
+    }
+];
